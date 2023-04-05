6e313e1fa1e3bde767d06f8e04259d622e061098
GabiDourado <129889799+GabiDourado@users.noreply.github.com>
2023-04-05 12:24:29 -0300

    restante das operaçoes

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ document.addEventListener("click",showcolor);
 document.getElementById("botao").addEventListener("click", exibenome);
 document.getElementById("soma").addEventListener("click", somando);
 document.getElementById("subtracao").addEventListener("click", subtraindo);
+document.getElementById("multiplicacao").addEventListener("click", multiplicando);
+document.getElementById("divisao").addEventListener("click", dividindo);
 function showcolor()
 {
     document.body.style.backgroundColor = "yellow";
@@ -24,10 +26,28 @@ function somando ()
 }
 function subtraindo ()
 {
-    let numero1 = document.getElementById("numero1").value;
-    let numero2 = document.getElementById("numero2").value;
-    let subtrai = parseFloat(numero1) - parseFloat(numero2);
+    let numeroo1 = document.getElementById("numero1").value;
+    let numeroo2 = document.getElementById("numero2").value;
+    let subtrai = parseFloat(numeroo1) - parseFloat(numeroo2);
     document.getElementById("resultado1").innerHTML = "= " + subtrai;
+    document.getElementById("numero1").value = "";
+    document.getElementById("numero2").value = "";
+}
+function multiplicando ()
+{
+    let numerooo1 = document.getElementById("numero-1").value;
+    let numerooo2 = document.getElementById("numero-2").value;
+    let multiplica = parseFloat(numerooo1) * parseFloat(numerooo2);
+    document.getElementById("resultado-1").innerHTML = "= " + multiplica;
+    document.getElementById("numero-1").value = "";
+    document.getElementById("numero-2").value = "";
+}
+function dividindo ()
+{
+    let numeroooo1 = document.getElementById("num-1").value;
+    let numeroooo2 = document.getElementById("num-2").value;
+    let divide = parseFloat(numeroooo1) / parseFloat(numeroooo2);
+    document.getElementById("result-1").innerHTML = "= " + divide;
     document.getElementById("numero1").value = "";
     document.getElementById("numero2").value = "";
 }
