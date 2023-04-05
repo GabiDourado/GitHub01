c7a0a32feba38e86509330fd31f43ddae7b7ba65
GabiDourado <129889799+GabiDourado@users.noreply.github.com>
2023-04-05 10:52:10 -0300

    mudanças javascript

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 document.addEventListener("click",showcolor);
 /*elemento - quando - ação*/
+document.getElementById("botao").addEventListener("click", exibenome);
 function showcolor()
 {
     document.body.style.backgroundColor = "red";
+}
+function exibenome()
+{
+    
 }
