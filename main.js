db2b7d633485e0069e4dd18716d35397b70e10e7
GabiDourado <129889799+GabiDourado@users.noreply.github.com>
2023-04-05 11:31:04 -0300

    voltei pro red

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ document.addEventListener("click",showcolor);
 document.getElementById("botao").addEventListener("click", exibenome);
 function showcolor()
 {
-    document.body.style.backgroundColor = "pink";
+    document.body.style.backgroundColor = "red";
 }
 function exibenome()
 {
