f7ce2341979dc95c4830181ad0e08a2b4d155533
GabiDourado <129889799+GabiDourado@users.noreply.github.com>
2023-04-05 11:50:43 -0300

    teste1

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 document.addEventListener("click",showcolor);
 /*elemento - quando - ação*/
 document.getElementById("botao").addEventListener("click", exibenome);
+document.getElementById("soma").addEventListener("click", somando)
 function showcolor()
 {
     document.body.style.backgroundColor = "#c8a2c8";
@@ -10,4 +11,12 @@ function exibenome()
     let usuario = document.getElementById("nome").value;
     document.getElementById("mensagem").innerHTML = "Meu nome é:" + usuario;
     document.getElementById("nome").value = "";
+}
+function somando ()
+{
+    let numero1 = document.getElementById("num1").value;
+    let numero2 = document.getElementById("num2").value;
+    document.getElementById("resultado").innerHTML = "=" + numero1 + numero2;
+    document.getElementById("num1").value = "";
+    document.getElementById("num2").value = "";
 }
